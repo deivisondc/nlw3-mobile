345aa1499972eec4743a2a081efe18bb0821205b
Deivison Cardoso <deivisondc@gmail.com>
2020-10-17 19:40:21 -0300

    connect create orphanage pages with api

--- src/pages/CreateOrphanage/OrphanageData/index.tsx
+++ src/pages/CreateOrphanage/OrphanageData/index.tsx
@@ -1,9 +1,86 @@
-import React from 'react';
-import { ScrollView, View, StyleSheet, Switch, Text, TextInput, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
+import { ScrollView, View, StyleSheet, Switch, Text, TextInput, TouchableOpacity, Image } from 'react-native';
 import { Feather } from '@expo/vector-icons';
 import { RectButton } from 'react-native-gesture-handler';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import { askAsync, CAMERA_ROLL } from 'expo-permissions'
+import * as ImagePicker from 'expo-image-picker';
+
+import api from '../../../services/api';
+
+interface OrphanageDataRouteParams {
+  position: {
+    latitude: number;
+    longitude: number;
+  };
+}
 
 const OrphanageData: React.FC = () => {
+  const [name, setName] = useState('');
+  const [about, setAbout] = useState('');
+  const [instructions, setInstructions] = useState('');
+  const [opening_hours, setOpeningHours] = useState('');
+  const [open_on_weekends, setOpenOnWeekends] = useState(true);
+  const [images, setImages] = useState<string[]>([]);
+
+  const navigation = useNavigation();
+  const route = useRoute();
+  const params = route.params as OrphanageDataRouteParams;
+
+  const handleSelectImages = async () => {
+    const { status } = await ImagePicker.getCameraRollPermissionsAsync();
+    let granted = status === 'granted';
+
+    if (!granted) {
+      const { status } = await askAsync(CAMERA_ROLL);
+
+      if (status !== 'granted') {
+        return;
+      }
+    }
+
+    const result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+    })
+
+    if (result.cancelled) {
+      return;
+    }
+
+    const { uri: image } = result;
+
+    setImages([...images, image ]);
+  }
+  
+  const handleCreateOrphanage = async () => {
+    const { latitude, longitude } = params.position;
+    console.log(latitude, longitude)
+
+    const data = new FormData();
+    data.append('name', name);
+    data.append('about', about);
+    data.append('latitude', String(latitude));
+    data.append('longitude', String(longitude));
+    data.append('instructions', instructions);
+    data.append('opening_hours', opening_hours);
+    data.append('open_on_weekends', String(open_on_weekends));
+
+    images.forEach((image, index) => {
+      data.append('images', {
+        name: `image_${index}.jpg`,
+        type: 'image/jpg', 
+        uri: image,
+      } as any)
+    })
+
+    // console.log('data', data)
+
+    await api.post('/orphanages', data).catch(err => console.log);
+    navigation.navigate('OrphanagesMap')
+  }
+
   return (
     <ScrollView style={styles.container} contentContainerStyle={{ padding: 24 }}>
       <Text style={styles.title}>Dados</Text>
@@ -11,12 +88,16 @@ const OrphanageData: React.FC = () => {
       <Text style={styles.label}>Nome</Text>
       <TextInput
         style={styles.input}
+        value={name}
+        onChangeText={setName}
       />
 
       <Text style={styles.label}>Sobre</Text>
       <TextInput
         style={[styles.input, { height: 110 }]}
         multiline
+        value={about}
+        onChangeText={setAbout}
       />
 
       <Text style={styles.label}>Whatsapp</Text>
@@ -25,7 +106,18 @@ const OrphanageData: React.FC = () => {
       />
 
       <Text style={styles.label}>Fotos</Text>
-      <TouchableOpacity style={styles.imagesInput} onPress={() => {}}>
+
+      <View style={styles.uploadedImagesContainer}>
+        {images.map(image => (
+          <Image
+            key={image}
+            source={{ uri: image }} 
+            style={styles.uploadedImage}
+          />
+        ))}
+      </View>
+
+      <TouchableOpacity style={styles.imagesInput} onPress={handleSelectImages}>
         <Feather name="plus" size={24} color="#15B6D6" />
       </TouchableOpacity>
 
@@ -35,11 +127,15 @@ const OrphanageData: React.FC = () => {
       <TextInput
         style={[styles.input, { height: 110 }]}
         multiline
+        value={instructions}
+        onChangeText={setInstructions}
       />
 
       <Text style={styles.label}>Horario de visitas</Text>
       <TextInput
         style={styles.input}
+        value={opening_hours}
+        onChangeText={setOpeningHours}
       />
 
       <View style={styles.switchContainer}>
@@ -47,10 +143,12 @@ const OrphanageData: React.FC = () => {
         <Switch 
           thumbColor="#fff" 
           trackColor={{ false: '#ccc', true: '#39CC83' }}
+          value={open_on_weekends}
+          onValueChange={setOpenOnWeekends}
         />
       </View>
 
-      <RectButton style={styles.nextButton} onPress={() => {}}>
+      <RectButton style={styles.nextButton} onPress={handleCreateOrphanage}>
         <Text style={styles.nextButtonText}>Cadastrar</Text>
       </RectButton>
     </ScrollView>
@@ -93,6 +191,18 @@ const styles = StyleSheet.create({
     paddingHorizontal: 24,
     marginBottom: 16,
     textAlignVertical: 'top',
+  },
+
+  uploadedImagesContainer: {
+    flexDirection: 'row'
+  },
+
+  uploadedImage: {
+    width: 64,
+    height: 64,
+    borderRadius: 20,
+    marginBottom: 32,
+    marginRight: 8,
   },
 
   imagesInput: {
